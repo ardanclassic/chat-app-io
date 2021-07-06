f5c6af2d3a8907b8abc08079992347ea67f2fa0b
IBNSHADIQ\ardanclassic <akbar@glovory.com>
2021-07-06 20:47:31 +0700

    update chat script

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,79 +1,92 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { useHistory } from 'react-router-dom'
-import { auth } from '../firebase'
-import { ChatEngine } from 'react-chat-engine'
-import { useAuth } from '../contexts/AuthContext'
+import React, { useRef, useState, useEffect } from "react"
+
 import axios from 'axios'
+import { useHistory } from "react-router-dom"
+import { ChatEngine } from 'react-chat-engine'
 
-const Chats = () => {
-  const history = useHistory();
+import { useAuth } from "../contexts/AuthContext"
+
+import { auth } from "../firebase"
+
+export default function Chats() {
   const didMountRef = useRef(false)
-  const { user } = useAuth();
+  const [ loading, setLoading ] = useState(true)
+  const { user } = useAuth()
+  const history = useHistory()
 
-  const [loading, setLoading] = useState(true)
-
-  const handleLogout = async () => {
-    await auth.signOut();
+  async function handleLogout() {
+    await auth.signOut()
     history.push("/")
   }
 
-  const getFile = async (url) => {
-    const response = await fetch(url)
-    const data = await response.blob()
-
-    return new File([data], "userPhoto.jpg", { type: "image/jpeg" })
+  async function getFile(url) {
+    let response = await fetch(url);
+    let data = await response.blob();
+    return new File([data], "test.jpg", { type: 'image/jpeg' });
   }
 
   useEffect(() => {
     if (!didMountRef.current) {
       didMountRef.current = true
+
       if (!user || user === null) {
         history.push("/")
-        return;
+        return
       }
-  
-      axios.get("https://api.chatengine.io/users/me/", {
-        headers: {
+      
+      // Get-or-Create should be in a Firebase Function
+      axios.get(
+        'https://api.chatengine.io/users/me/',
+        { headers: { 
           "project-id": process.env.REACT_APP_CHAT_ENGINE_ID,
           "user-name": user.email,
-          "user-secret": user.uid, 
-        }
-      }).then(() => {
-        setLoading(false)
-      }).catch(() => {
-        let formData = new FormData();
-        formData.append("email", user.email)
-        formData.append("username", user.email)
-        formData.append("secret", user.uid)
-  
-        getFile(user.photoURL).then((avatar) => {
-          formData.append("avatar", avatar, avatar.name)
-          axios.post("https://api.chatengine.io/users/", 
-            formData, 
-            { headers: { "private-key": process.env.REACT_APP_CHAT_ENGINE_KEY } }
+          "user-secret": user.uid
+        }}
+      )
+
+      .then(() => setLoading(false))
+
+      .catch(e => {
+        let formdata = new FormData()
+        formdata.append('email', user.email)
+        formdata.append('username', user.email)
+        formdata.append('secret', user.uid)
+
+        getFile(user.photoURL)
+        .then(avatar => {
+          formdata.append('avatar', avatar, avatar.name)
+
+          axios.post(
+            'https://api.chatengine.io/users/',
+            formdata,
+            { headers: { "private-key": process.env.REACT_APP_CHAT_ENGINE_KEY }}
           )
           .then(() => setLoading(false))
-          .catch((error) => console.log(error))
+          .catch(e => console.log('e', e.response))
         })
       })
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     }
   }, [user, history])
+  
 
-  if (!user || loading) return 'Loading . . .'
+  if (!user || loading) return <div />
 
   return (
-    <div className="chats-page">
-      <div className="nav-bar">
-        <div className="logo-tab">
-          Chat-App
+    <div className='chats-page'>
+      <div className='nav-bar'>
+        <div className='logo-tab'>
+          Unichat
         </div>
-        <div className="logout-tab" onClick={handleLogout}>
+
+        <div onClick={handleLogout} className='logout-tab'>
           Logout
         </div>
       </div>
 
       <ChatEngine 
-        height="calc(100vh - 66px)"
+        height='calc(100vh - 66px)'
         projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
         userName={user.email}
         userSecret={user.uid}
@@ -81,5 +94,3 @@ const Chats = () => {
     </div>
   )
 }
-
-export default Chats
